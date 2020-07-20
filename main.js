54f656f0873f13e85bd08538577e5cea9a0ac1e2
Jonathon Joyner <jonathonjoyner@Jonathons-MacBook-Pro.local>
2020-07-20 18:51:57 -0500

    tested data and primtive types

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log("js-intro");
+// * undefined 
+// let x; 
+// console.log(x);
+
+//* boolean 
+// let x = true;
+// let y = false;
+
+// console.log(x, y)
+
+// if (!y) {
+//   console.log("this is true");
+// } else {
+//   console.log("this is false")
+// }
+
+//* number 
+
+// let numbe
+// r = 22;
+  
+
+// let string = "Today is Monday"
+
+// if (string === 'Today is Monday') {
+//   console.log('YaYAyAya')
+// }
+// console.log(typeof string)
+
+// let name = 'jon',
+//     age = "haha",
+//     school = "NSS";
+
+//     let object = {
+//       name,
+//       age,
+//       school
+//     }
+
+//     console.log(object.name)
