5ed13b1494e1f50689b744f9aa847263be6e4d27
Jonathon Joyner <jonathonjoyner@Jonathons-MacBook-Pro.local>
2020-07-20 19:57:08 -0500

    created function

--- functions.js
+++ functions.js
@@ -1,1 +1,24 @@
-console.log("functions");
+// console.log("functions");
+
+// const nuggetizer = (animal) => {
+//   return `processed ${animal}`
+// }
+
+// console.log(nuggetizer('pig'));
+// console.log(nuggetizer('fish'));
+// console.log(nuggetizer('seitan'));
+// console.log(nuggetizer('eggplant'));
+// console.log(nuggetizer('chicken'));
+
+
+// const pabloescobar = (number)  => {
+//   return number + 42
+// }
+// console.log(pabloescobar(10));
+
+const iphone99SPlus = (year) => {
+  return 2099 - year
+}
+console.log(iphone99SPlus(1998));
+
+
